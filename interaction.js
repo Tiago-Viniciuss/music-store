038a2b7592c965b1fc5af43a58ed807335b21fa2
Tiago Vinícius <tiagovinicyus1996@gmail.com>
2023-02-26 21:17:55 +0000

    More sections added
    I made the sections acoustic and drumms and improved some stuffs

--- interaction.js
+++ interaction.js
@@ -6,14 +6,6 @@ function openMenu () {
     menu.classList.toggle('active')
 }
 
-const observer = new IntersectionObserver(entries => {
-    console.log(entries)
-    entries[0].target.classList.add('init-hidden-off')
-},)
-
-Array.from(document.querySelectorAll('.init-hidden')).forEach(element => {
-    observer.observe(element)
-})
 
 
   
