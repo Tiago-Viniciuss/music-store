bb04602615e040c86779ea81445e974913031af9
Tiago Vinícius <tiagovinicyus1996@gmail.com>
2023-02-26 16:09:27 +0000

    update
    I tried to do a animation when the page scrolls, I will see if it works

--- interaction.js
+++ interaction.js
@@ -6,6 +6,14 @@ function openMenu () {
     menu.classList.toggle('active')
 }
 
+const observer = new IntersectionObserver(entries => {
+    console.log(entries)
+    entries[0].target.classList.add('init-hidden-off')
+},)
+
+Array.from(document.querySelectorAll('.init-hidden')).forEach(element => {
+    observer.observe(element)
+})
 
 
   
